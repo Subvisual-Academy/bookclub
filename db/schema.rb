6fb75e40685eebe1353c6c1debf19671937b265c
jpcorreia99 <jpcorreia99@gmail.com>
2020-08-03 14:23:45 +0100

    fix schema.rb conflict

--- db/schema.rb
+++ db/schema.rb
@@ -15,15 +15,6 @@ ActiveRecord::Schema.define(version: 2020_07_30_094955) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "books", force: :cascade do |t|
-    t.string "title"
-    t.string "author"
-    t.text "synopsis"
-    t.string "image"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
     t.string "name", null: false
