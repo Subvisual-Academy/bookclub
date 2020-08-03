ef67e89d38c4a596a8b1e74e7663e6f5fde10f80
jpcorreia99 <jpcorreia99@gmail.com>
2020-08-03 14:20:26 +0100

    implement requested changes

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,13 +1,12 @@
 require "rails_helper"
 
 RSpec.describe User, type: :model do
-  it { is_expected.to validate_presence_of(:name) }
-  it { is_expected.to validate_presence_of(:email) }
+  subject{ build(:user) }
 
-  it "it validates the uniqueness of the email field" do
-    create(:user)
-    is_expected.to validate_uniqueness_of(:email)
-  end
+  it { is_expected.to validate_presence_of(:name) }
+
+  it { is_expected.to validate_presence_of(:email) }
+  it { is_expected.to validate_uniqueness_of(:email) }
 
   it { is_expected.to validate_presence_of(:password) }
   it { is_expected.to validate_length_of(:password) }
