80620d117b9f92be803ee6ec134a5b03bce65dd5
jpcorreia99 <jpcorreia99@gmail.com>
2020-08-03 18:34:08 +0100

    Rubocop

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
-
   def not_authenticated
     redirect_to login_path
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 RSpec.describe User, type: :model do
-  subject{ build(:user) }
+  subject { build(:user) }
 
   it { is_expected.to validate_presence_of(:name) }
 
