c75f333feef168222f92ba0bca349a32eccf80ba
Gil Afonso <gil.lomba.afonso@gmail.com>
2020-08-06 14:25:55 +0100

    implement requested changes

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby "2.6.6"
 
 gem "bootsnap", ">= 1.4.2", require: false
+gem "figaro"
 gem "jbuilder", "~> 2.7"
 gem "pg", ">= 0.18", "< 2.0"
 gem "puma", "~> 4.1"
--- config/initializers/slack.rb
+++
@@ -1,4 +0,0 @@
-require "slack-notifier"
-
-Slack_Notifier = Slack::Notifier.new "https://hooks.slack.com/services/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",
-                                     channel: "#bot_testing", username: "BookClub-bot"
