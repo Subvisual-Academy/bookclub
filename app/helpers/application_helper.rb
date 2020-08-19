8f326e67a3ecdf090904149b1953a5f19fdfe340
jpcorreia99 <jpcorreia99@gmail.com>
2020-08-19 10:59:02 +0100

    add next gatheirng date information to gatherings index

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,23 @@
 module ApplicationHelper
+  def next_gathering_date
+    this_month_gathering = find_this_month_gathering
+
+    if Time.zone.today <= this_month_gathering
+      this_month_gathering.strftime("%A, %d %B %Y")
+    else
+      find_next_month_gathering.strftime("%A, %d %B %Y")
+    end
+  end
+
+  private
+
+  def find_this_month_gathering
+    Time.zone.today.at_beginning_of_month.
+      next_occurring(:thursday).next_occurring(:thursday).next_occurring(:thursday)
+  end
+
+  def find_next_month_gathering
+    Time.zone.today.at_beginning_of_month.next_month.
+      next_occurring(:thursday).next_occurring(:thursday).next_occurring(:thursday)
+  end
 end
