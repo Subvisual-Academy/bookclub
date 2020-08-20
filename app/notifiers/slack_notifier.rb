c31eb3df1c73a58c7440b4555dc04d42a707ee4e
jpcorreia99 <jpcorreia99@gmail.com>
2020-08-20 14:06:03 +0100

    move slack_notifier variables inside the methods

--- app/notifiers/slack_notifier.rb
+++ app/notifiers/slack_notifier.rb
@@ -1,16 +1,21 @@
 module SlackNotifier
-  @slack_notifier = Slack::Notifier.new ENV["SLACK_URL"], channel: ENV["SLACK_CHANNEL"], username: "BookClub-bot"
 
   class << self
     def notify_minute(date, url)
+      @slack_notifier ||= Slack::Notifier.new ENV["SLACK_URL"], channel: ENV["SLACK_CHANNEL"], username: "BookClub-bot"
+
       @slack_notifier.ping(text: "*#{date.strftime('%d %B %Y')}*: This month's books are available on: #{url} :book:")
     end
 
     def notify_start_of_week
+      @slack_notifier ||= Slack::Notifier.new ENV["SLACK_URL"], channel: ENV["SLACK_CHANNEL"], username: "BookClub-bot"
+
       @slack_notifier.ping(text: "*Reminder*: The Bookclub gathering is this week :book:")
     end
 
     def notify_day
+      @slack_notifier ||= Slack::Notifier.new ENV["SLACK_URL"], channel: ENV["SLACK_CHANNEL"], username: "BookClub-bot"
+
       @slack_notifier.ping(text: "*Reminder*: The Bookclub gathering is today :book:")
     end
   end
