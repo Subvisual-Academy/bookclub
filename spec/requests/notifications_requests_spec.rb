69d21c455fbd1977cf0876fe6fc32fb0a3850148
João Correia <46303606+jpcorreia99@users.noreply.github.com>
2020-08-27 17:02:18 +0100

    Create separate "send notification" button (#18)
    This PR implements the following features:
    
    - Creating a gathering no longer send a slack notification.
     - In the show page, show a button, only available to the boocklub moderator (decided through the moderator db column), which let's him send a notification to slack.

---
+++ spec/requests/notifications_requests_spec.rb
@@ -0,0 +1,24 @@
+require "rails_helper"
+
+RSpec.describe "Notifications", type: :request do
+  describe "POST #create" do
+    it "returns unauthorized if user is not moderator" do
+      login_user(create(:user))
+      gathering = create(:gathering)
+
+      post gathering_notifications_path(gathering), params: { gathering_id: gathering.id }
+
+      expect(response).to have_http_status(:unauthorized)
+    end
+
+    it "redirects to gatherings_path on a successful notification" do
+      allow(SlackNotifier).to receive(:notify_minute).and_return(nil)
+      login_user(create(:user, :is_moderator))
+      gathering = create(:gathering)
+
+      post gathering_notifications_path(gathering), params: { gathering_id: gathering.id }
+
+      expect(response).to redirect_to(gatherings_path)
+    end
+  end
+end
