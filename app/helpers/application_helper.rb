bddeda52fe4db44a2480369ed589c1b01b856deb
jpcorreia99 <jpcorreia99@gmail.com>
2020-08-28 19:41:06 +0100

    display name and title when editing

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def convert_user_id_to_name(user_id)
+    User.find(user_id).name if user_id
+  end
+
+  def convert_book_id_to_title(book_id)
+    Book.find(book_id).title if book_id
+  end
 end
