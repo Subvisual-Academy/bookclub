d826afff22c70ee46b72d3b252340e9830bf4ea7
José Sousa <josesousa9000@gmail.com>
2020-09-01 19:32:17 +0100

    Add linters

--- app/javascript/controllers/nested_form_controller.js
+++ app/javascript/controllers/nested_form_controller.js
@@ -1,18 +1,21 @@
-import { Controller } from "stimulus"
+import { Controller } from "stimulus";
 
 export default class extends Controller {
-  static targets = ["add_item", "template"]
+  static targets = ["addItem", "template"];
 
   addAssociation(event) {
-    event.preventDefault()
-    const content = this.templateTarget.innerHTML.replace(/TEMPLATE_RECORD/g, new Date().valueOf())
-    this.add_itemTarget.insertAdjacentHTML('beforebegin', content)
+    event.preventDefault();
+    const content = this.templateTarget.innerHTML.replace(
+      /TEMPLATE_RECORD/g,
+      new Date().valueOf()
+    );
+    this.addItemTarget.insertAdjacentHTML("beforebegin", content);
   }
 
   removeAssociation(event) {
-    event.preventDefault()
-    const item = event.target.closest(".nested-fields")
-    item.querySelector("input[name*='_destroy']").value = 1
-    item.style.display = 'none'
+    event.preventDefault();
+    const item = event.target.closest(".nested-fields");
+    item.querySelector("input[name*='_destroy']").value = 1;
+    item.style.display = "none";
   }
 }
