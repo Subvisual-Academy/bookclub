365d2c3cf83cc3f5a713b8acf9babc9536208a16
Gil Afonso <gil.lomba.afonso@gmail.com>
2020-09-02 16:27:30 +0100

    Change sidebar according to current page

--- app/javascript/sidebar.js
+++ app/javascript/sidebar.js
@@ -1,12 +1,44 @@
-var hidden = true;
-document.toggleSidebar = (event) => {
-    const sidebarElement = event.currentTarget;
-    if (hidden) {
-        sidebarElement.style.width = "21.5%";
-        hidden = false;
-    } else {
-        sidebarElement.style.width = "5%";
-        sidebarElement.style.paddingLeft = "1.2%";
-        hidden = true;
+window.addEventListener("DOMContentLoaded", () => {
+    const sidebarElement = document.querySelector("#sidebar");
+    let afterNavigation = false;
+  
+    document.addEventListener("turbolinks:visit", () => {
+      afterNavigation = true;
+  
+      closeSidebar();
+    });
+  
+    sidebarElement.addEventListener("mouseover", () => {
+      if (afterNavigation) return;
+  
+      const mouseIsInSidebar = sidebarElement.contains(getElementUnderMouse());
+  
+      if (!mouseIsInSidebar) return;
+  
+      openSidebar();
+    });
+  
+    sidebarElement.addEventListener("mouseout", () => {
+      const mouseIsInSidebar = sidebarElement.contains(getElementUnderMouse());
+  
+      if (mouseIsInSidebar) return;
+  
+      afterNavigation = false;
+      closeSidebar();
+    });
+  
+    function closeSidebar() {
+      sidebarElement.style.width = "5%";
+      sidebarElement.style.paddingLeft = "1.2%";
     }
-}
+  
+    function openSidebar() {
+      sidebarElement.style.width = "21.5%";
+    }
+  
+    function getElementUnderMouse() {
+      const nodes = document.querySelectorAll(":hover");
+  
+      return nodes[nodes.length - 1];
+    }
+});
