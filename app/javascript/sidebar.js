747670cd77bdd8515dd9d97a9488d5c8b0a6c1c2
Gil Afonso <gil.lomba.afonso@gmail.com>
2020-09-02 08:55:00 +0100

    Correct indentation and move js file

---
+++ app/javascript/sidebar.js
@@ -0,0 +1,12 @@
+var hidden = true;
+document.toggleSidebar = (event) => {
+    const sidebarElement = event.currentTarget;
+    if (hidden) {
+        sidebarElement.style.width = "20%";
+        hidden = false;
+    } else {
+        sidebarElement.style.width = "5%";
+        sidebarElement.style.paddingLeft = "1%";
+        hidden = true;
+    }
+}
