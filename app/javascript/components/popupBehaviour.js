30a9cab6aa4f06a4c6770f8843434ea747e5842c
jpcorreia99 <jpcorreia99@gmail.com>
2020-09-09 11:36:24 +0100

    run linters

--- app/javascript/components/popupBehaviour.js
+++ app/javascript/components/popupBehaviour.js
@@ -1,24 +1,24 @@
 document.addEventListener("turbolinks:load", addPopupAction);
 
 function addPopupAction() {
-    const popupDivs = document.querySelectorAll(".Books-popup");
+  const popupDivs = document.querySelectorAll(".Books-popup");
 
-    popupDivs.forEach((popupDiv) => {
-        popupDiv.addEventListener("click", function showPopup() {
-            const popupContent = popupDiv.querySelectorAll(".Books-popuptext")[0];
-            popupContent.classList.toggle("Books-show");
-            closeOtherPopups(popupDiv);
-        })
+  popupDivs.forEach((popupDiv) => {
+    popupDiv.addEventListener("click", function showPopup() {
+      const popupContent = popupDiv.querySelectorAll(".Books-popuptext")[0];
+      popupContent.classList.toggle("Books-show");
+      closeOtherPopups(popupDiv);
     });
+  });
 }
 
 function closeOtherPopups(currentPopup) {
-    const popupDivs = document.querySelectorAll(".Books-popup");
+  const popupDivs = document.querySelectorAll(".Books-popup");
 
-    popupDivs.forEach((popupDiv) => {
-        if(popupDiv !== currentPopup) {
-            const popupContent = popupDiv.querySelectorAll(".Books-popuptext")[0];
-            popupContent.classList.remove("Books-show");
-        }
-    });
+  popupDivs.forEach((popupDiv) => {
+    if (popupDiv !== currentPopup) {
+      const popupContent = popupDiv.querySelectorAll(".Books-popuptext")[0];
+      popupContent.classList.remove("Books-show");
+    }
+  });
 }
