43d467f1606d430a9d91c1556cec7002cd337662
jpcorreia99 <jpcorreia99@gmail.com>
2020-09-11 10:42:24 +0100

    change message

--- app/notifiers/slack_notifier.rb
+++ app/notifiers/slack_notifier.rb
@@ -3,7 +3,7 @@ module SlackNotifier
     def notify_minute(date, url)
       @slack_notifier ||= Slack::Notifier.new ENV["SLACK_URL"], channel: ENV["SLACK_CHANNEL"], username: ENV["BOT_NAME"]
 
-      @slack_notifier.ping(text: "The books for the *#{date.strftime('%d %B, %Y')}* gathering are now available! Check them out on #{url} :book:")
+      @slack_notifier.ping(text: "The books from the *#{date.strftime('%B %-dth')}* gathering are now available! Check them out on: #{url} :book:")
     end
 
     def notify_start_of_week
