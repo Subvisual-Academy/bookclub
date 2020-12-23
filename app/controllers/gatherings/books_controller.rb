d26fb697f481a1a3ca0b1d443aba3e56bed4a33d
Francisco Sousa <francisco.sousa@hey.com>
2020-12-23 23:03:37 +0000

    Move gathering date to partial

--- app/controllers/gatherings/books_controller.rb
+++ app/controllers/gatherings/books_controller.rb
@@ -2,5 +2,6 @@ class Gatherings::BooksController < ApplicationController
   def index
     @gathering = Gathering.find(params[:gathering_id])
     @books = @gathering.books.search(params[:search])
+    @books = @books.left_joins(:book_presentations).where("book_presentations.user_id": params[:user_id]) if params[:user_id]
   end
 end
